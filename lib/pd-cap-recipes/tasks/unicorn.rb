db2a4f67c1519390436ccefb3f091e0b30bf0df3
Ian Enders <ian.enders@gmail.com>
2012-05-07 16:09:01 -0700

    Stage to rails env

--- lib/pd-cap-recipes/tasks/unicorn.rb
+++ lib/pd-cap-recipes/tasks/unicorn.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
           desc "#{action} Unicorn"
           task action.to_sym, :roles => role, :on_error => :continue do
             # Use nohup because otherwise nginx dies when the connection is severed 
-            run  "cd ${RAILS_ROOT} && RAILS_ENV=#{stage} APP=#{application} sudo nohup /etc/init.d/#{unicorn_init_script} #{action}"
+            run  "cd ${RAILS_ROOT} && RAILS_ENV=#{rails_env} APP=#{application} sudo nohup /etc/init.d/#{unicorn_init_script} #{action}"
           end
         end
       end
