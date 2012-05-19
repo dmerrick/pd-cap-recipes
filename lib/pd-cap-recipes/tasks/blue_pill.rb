790789a5f1e87941ededddb920ef91a63c00dd5c
Simon Mathieu <simon.math@gmail.com>
2012-05-18 17:28:30 -0700

    Bluepill fix

--- lib/pd-cap-recipes/tasks/blue_pill.rb
+++ lib/pd-cap-recipes/tasks/blue_pill.rb
@@ -1,42 +1,3 @@
-def bundle_exec(cmd, options = {})
-  options = options.clone
-  to_run = "sh -c \"cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec #{cmd}\""
-  to_run << ' || true' if options.delete(:no_error)
-  output = ""
-  append_to_output = lambda do |channel, stream, data|
-    level = stream == :err ? :important : :info
-    logger.send(level, data, "#{stream} :: #{channel[:server]}")
-    output << data 
-  end
-
-  if options.delete(:sudo)
-    sudo to_run, options, &append_to_output
-  else
-    run to_run, options, &append_to_output
-  end
-  output
-end
-
-def bluepill_exec(cmd, options = {})
-  bundle_exec "bluepill #{cmd}", {:sudo => true, :no_error => true}.merge(options)
-end
-
-def bluepill_path
-  File.join(current_path, 'config', 'bluepill.pill')
-end
-
-def bluepill_started?(hosts)
-  # Command to check that a process is running. Should be somethink like:
-  # ps ax | grep 'my_worker\s*$' | wc -l 
-  cmd = fetch(:bluepill_started_cmd, 'true')
-  workers = capture(cmd, :hosts => hosts)
-  workers.to_i > 0
-end
-
-def rolling_timeout?(time_started)
-  time_started + 2.minutes < Time.now 
-end
-
 Capistrano::Configuration.instance(:must_exist).load do |config|
   namespace :bluepill do
     desc "Stop processes that bluepill is monitoring and quit bluepill"
@@ -79,4 +40,44 @@ Capistrano::Configuration.instance(:must_exist).load do |config|
       bluepill_exec "status"
     end
   end
+
+  def bundle_exec(cmd, options = {})
+    options = options.clone
+    to_run = "sh -c \"cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec #{cmd}\""
+    to_run << ' || true' if options.delete(:no_error)
+    output = ""
+    append_to_output = lambda do |channel, stream, data|
+      level = stream == :err ? :important : :info
+      logger.send(level, data, "#{stream} :: #{channel[:server]}")
+      output << data 
+    end
+
+    if options.delete(:sudo)
+      sudo to_run, options, &append_to_output
+    else
+      run to_run, options, &append_to_output
+    end
+    output
+  end
+
+  def bluepill_exec(cmd, options = {})
+    bundle_exec "bluepill #{cmd}", {:sudo => true, :no_error => true}.merge(options)
+  end
+
+  def bluepill_path
+    File.join(current_path, 'config', 'bluepill.pill')
+  end
+
+  def bluepill_started?(hosts)
+    # Command to check that a process is running. Should be somethink like:
+    # ps ax | grep 'my_worker\s*$' | wc -l 
+    cmd = fetch(:bluepill_started_cmd, 'true')
+    workers = capture(cmd, :hosts => hosts)
+    workers.to_i > 0
+  end
+
+  def rolling_timeout?(time_started)
+    time_started + 2.minutes < Time.now 
+  end
+
 end
